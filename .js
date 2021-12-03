01e7153a9162f9cfe2a15d9aaee116dc68141668
Shea Challis <93648478+sheachallis@users.noreply.github.com>
2021-12-03 12:19:25 +0000

    Create .js

---
+++ .js
@@ -0,0 +1,10 @@
+
+let numberOne = 89;
+let numberTwo = 34;
+
+function highestNumber() {
+    if (numberOne > numberTwo) console.log(numberOne);
+    else console.log(numberTwo);
+}
+
+highestNumber();
